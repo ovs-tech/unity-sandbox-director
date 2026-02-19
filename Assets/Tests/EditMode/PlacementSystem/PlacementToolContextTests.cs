2afac29889ace8a541308747448947f5f5eaafc3
siduko <ace.siduko@gmail.com>
2026-02-19 08:52:05 +0700

    test: strengthen placement tool context test

--- Assets/Tests/EditMode/PlacementSystem/PlacementToolContextTests.cs
+++ Assets/Tests/EditMode/PlacementSystem/PlacementToolContextTests.cs
@@ -1,12 +1,35 @@
 using NUnit.Framework;
 using Systems.PlacementSystem.Tools;
+using UnityEngine;
 
 public class PlacementToolContextTests
 {
     [Test]
     public void Context_IsReadOnly_PropertiesAreAssigned()
     {
-        var context = new PlacementToolContext(null, null, null, null, null, null, 0f, 0f, 0f, false);
+        var cameraObject = new GameObject("TestCamera");
+        var camera = cameraObject.AddComponent<Camera>();
+
+        var context = new PlacementToolContext(
+            null,
+            null,
+            null,
+            null,
+            null,
+            camera,
+            12.5f,
+            45f,
+            90f,
+            true);
+
         Assert.NotNull(context);
+        Assert.AreEqual(camera, context.PlacementCamera);
+        Assert.AreEqual(12.5f, context.MaxRaycastDistance);
+        Assert.AreEqual(45f, context.RotationIncrementDegrees);
+        Assert.AreEqual(90f, context.RotationSnapDegrees);
+        Assert.IsTrue(context.AllowMultiSelection);
+        Assert.NotNull(context.SelectionState);
+
+        Object.DestroyImmediate(cameraObject);
     }
 }
