c1a1c2c0ec9605881f0a1e126845537c4d19567f
siduko <ace.siduko@gmail.com>
2026-04-05 09:16:02 +0700

    feat(UI): add login and welcome screens with associated styles and navigation support

--- Assets/Scripts/UI/Screens/Welcome/WelcomeNavigationScreen.cs
+++ Assets/Scripts/UI/Screens/Welcome/WelcomeNavigationScreen.cs
@@ -6,23 +6,22 @@ using UnityEngine.UIElements;
 class WelcomeNavigationScreen : NavigationScreen
 {
   private readonly NavHost host;
-  private readonly Button loginButton;
+  private Button loginButton;
+
   public WelcomeNavigationScreen(VisualTreeAsset uxmlAsset, NavHost host)
   {
     this.host = host;
-    style.flexDirection = FlexDirection.Column;
     uxmlAsset.CloneTree(this);
-    // find login button and add click handler to navigate to login screen
-    loginButton = this.Q<Button>("login-link-button");
   }
 
   public override void OnEnter(NavController controller, NavDestination destination, Argument[] args)
   {
     base.OnEnter(controller, destination, args);
 
-    // loginButton.clicked += () =>
-    // {
-    //   host.navController.Navigate("login");
-    // };
+    this.loginButton = this.Q<Button>("login-link-button");
+    loginButton.clicked += () =>
+    {
+      host.navController.Navigate("login");
+    };
   }
 }
